5ff1d28f78947cfcee2d01898470fdf78d10374a
subzero <shubham8111994@gmail.com>
2023-05-05 23:05:08 +0530

    updated to calculate parttime employee wage

--- EmployeeWage.swift
+++ EmployeeWage.swift
@@ -1,13 +1,18 @@
 let IS_FULL_TIME = 1
+let IS_PART_TIME = 2
 let RATE_PER_HOUR = 20
 var empHrs: Int = 0
 var empWage: Int = 0
-var empCheck: Int = Int.random(in: 0...1)
+var empCheck: Int = Int.random(in: 0...2)
 
 if empCheck == IS_FULL_TIME{
     empHrs = 8    
+} else if empCheck == IS_PART_TIME{
+    empHrs = 4
 } else {
     empHrs = 0
 }
 empWage = empHrs * RATE_PER_HOUR
 print("empWage: \(empWage)")
+
+
