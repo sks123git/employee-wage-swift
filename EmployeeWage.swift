a3742e4104f46fea9996635e50bf918afed97c20
subzero <shubham8111994@gmail.com>
2023-05-05 18:37:50 +0530

    updated to calculate employee wage

--- EmployeeWage.swift
+++ EmployeeWage.swift
@@ -1,7 +1,13 @@
 let IS_FULL_TIME = 1
+let RATE_PER_HOUR = 20
+var empHrs: Int = 0
+var empWage: Int = 0
 var empCheck: Int = Int.random(in: 0...1)
+
 if empCheck == IS_FULL_TIME{
-    print("employee is present")
+    empHrs = 8    
 } else {
-    print("employee is absent")
+    empHrs = 0
 }
+empWage = empHrs * RATE_PER_HOUR
+print("empWage: \(empWage)")
